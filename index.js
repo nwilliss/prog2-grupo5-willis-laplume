e909bf7316bf13d47dbf3d364986ee04d135837a
vlaplume <vlaplume@udesa.edu.ar>
2024-04-07 20:35:13 -0300

    modificar rutas de links

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express();
 
 const indexController = require("./controllers/indexController");
 
-app.use("/",indexController);
+// app.use("/",indexController);
+
+app.use(express.static(__dirname + "/public")); //variable de cual es mi directorio actual 
 
 app.set("view engine", "ejs");
+
+app.get("/", (req,res)=> {
+    res.render("index"); 
+});
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
