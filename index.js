90704dba509d8a5cc8a547b561aba2e5c22ecd35
vlaplume <vlaplume@udesa.edu.ar>
2024-04-07 22:55:00 -0300

    formulario register

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use(express.static(__dirname + "/public")); //variable de cual es mi directo
 
 app.set("view engine", "ejs");
 
-app.get("/", (req,res)=> {
-    res.render("index"); 
-});
+// app.get("/", (req,res)=> {
+//     res.render("index"); 
+// });
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
