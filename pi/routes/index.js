49fd1d42ca235a56cd2dfaa8dea5f6315ec2f817
vlaplume <vlaplume@udesa.edu.ar>
2024-04-19 12:50:46 -0300

    not found

--- pi/routes/index.js
+++ pi/routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 
 module.exports = router;
