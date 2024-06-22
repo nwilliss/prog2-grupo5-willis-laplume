d7d7fd905ae57724440cf5785a09fafc42c1648c
vlaplume <vlaplume@udesa.edu.ar>
2024-06-22 20:00:07 -0300

    punto8

--- pi/controllers/productController.js
+++ pi/controllers/productController.js
@@ -2,6 +2,7 @@ const { or, where } = require("sequelize");
 var db = require("../database/models");
 const Producto = db.Producto;
 const Op = db.Sequelize.Op; 
+const { validationResults } = require("express-validator");
 
 var productController = {
   product: function (req, res) {
@@ -15,8 +16,33 @@ var productController = {
     res.render("product", { productos: db.productos });
   },
 
+  validatedAdd: function (req,res) {
+    if (req.session.user !== undefined){
+      return res.render ("product-add", {errors :[] })
+    }else {
+      return res.render("/login")
+    }
+  },
+
   add: function (req, res) {
-    res.render("product-add");
+    //res.render("product-add");
+    let errors = validationResult(req);
+
+      if (errors.isEmpty()) {
+        let producto = req.body;
+        producto.usuarioId = req.session.user.id;
+        Producto.create(producto)
+        .then(function(response){
+          return res.redirect("/")
+
+        })
+        .catch(function(error){
+          return res.send(error)
+
+        })
+      } else {
+        return res.render("product-add", {errors : errors })
+      }
   },
 
   search: function (req, res) {
--- pi/routes/products.js
+++ pi/routes/products.js
@@ -1,8 +1,17 @@
 var express = require("express");
 var router = express.Router();
 var productController = require("../controllers/productController");
+const {body} = require ("express-validator");
 
-router.get("/product-add", productController.add);
+let addValidation = [
+
+    body("imagen").notEmpty().withMessage("Debes subir una imagen"),
+    body("nombre").notEmpty.withMessage("Debes completar el nombre"),
+    body("descripcion").notEmpty.withMessage("Debes completar la descripción"),
+];
+
+router.get("/product-add", productController.validateAdd);
+router.post("/product-add",addValidatio, productController.add);
 
 router.get("/buscador", productController.search);
 
