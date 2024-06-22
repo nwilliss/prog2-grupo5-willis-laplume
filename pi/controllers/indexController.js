ea01970aee921b9b2705711b864a7b0028f2bb0b
vlaplume <vlaplume@udesa.edu.ar>
2024-06-22 19:13:41 -0300

    error

--- pi/controllers/indexController.js
+++ pi/controllers/indexController.js
@@ -3,9 +3,7 @@ const indexController = {
     index: function(req, res) {
         res.render('index');
     },
-    search: function(req, res) {
-        res.render('search-results');
-    }
+  
 }
 
 module.exports = indexController;
